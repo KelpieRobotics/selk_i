c521733b73a8bb9ed8c0c4200a8130a36b250a9c
Fufs <42976807+Fufs@users.noreply.github.com>
2025-01-18 14:55:38 -0500

    Initialize package and add basic view_bot.launch.py

---
+++ launch/nodes/rviz.launch.py
@@ -0,0 +1,42 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    pkg_share = get_package_share_directory('selk_i')
+
+    config_file = LaunchConfiguration("config_file")
+    namespace = LaunchConfiguration("namespace")
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', PathJoinSubstitution([pkg_share, "rviz", config_file])],
+        namespace=namespace,
+        remappings=[('/tf','tf'),('/tf_static','tf_static')], # Remap tf topics to the namespace
+    )
+
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'config_file',
+            default_value=os.path.join(pkg_share, "rviz", "dev.rviz"),
+            description="RVIZ configuration file"
+        ),
+        DeclareLaunchArgument(
+            'namespace',
+            default_value="/selk_i",
+            description="Namespace of the bot (usually its unique identifier)"
+        ),
+
+        rviz_node
+    ])
---
+++ launch/view_bot.launch.py
@@ -0,0 +1,50 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+import xacro
+
+def generate_launch_description():
+
+    pkg_share = get_package_share_directory('selk_i')
+
+    namespace = LaunchConfiguration("namespace")
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'namespace',
+            default_value="/selk_i",
+            description="Namespace of the bot (usually its unique identifier)"
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                os.path.join(pkg_share, "launch", "nodes", "robot_state_publisher.launch.py")
+            ]),
+            launch_arguments=[
+                ("namespace", namespace),
+            ],
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                os.path.join(pkg_share, "launch", "nodes", "rviz.launch.py")
+            ]),
+            launch_arguments=[
+                ("config_file", os.path.join(pkg_share, "rviz", "view_bot.rviz")),
+                ("namespace", namespace),
+            ],
+        ),
+# TODO: Camera Joints
+#        IncludeLaunchDescription(
+#            PythonLaunchDescriptionSource([
+#                os.path.join(pkg_share, "launch", "nodes", "joint_state_publisher_gui.launch.py")
+#            ]),
+#            launch_arguments=[
+#                ("namespace", namespace),
+#            ],
+#        ),
+    ])
